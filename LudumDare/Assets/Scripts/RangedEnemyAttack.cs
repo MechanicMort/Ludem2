47a57328760465b279b8a7f3db66aab2abba4c47
Alex Smith <50043899+AlexSmith292@users.noreply.github.com>
2019-04-27 19:26:21 +0100

    ai

---
+++ LudumDare/Assets/Scripts/RangedEnemyAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemyAttack : MonoBehaviour {
+
+    public GameObject goPlayer;
+    private Vector3 playerVector;
+    private Vector3 enemyVector;
+    private float dist;
+    public GameObject goProjectile;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerVector = goPlayer.transform.position;
+        enemyVector = this.transform.position;
+        dist = Vector3.Distance(playerVector, enemyVector);
+        if (dist < 0.95f)
+        {
+
+        }
+	}
+}
