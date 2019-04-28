720ceee6158aa9885dd67a67f90713de02984040
Alex Smith <50043899+AlexSmith292@users.noreply.github.com>
2019-04-28 13:31:59 +0100

    items

---
+++ LudumDare/Assets/Scripts/ShopScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour {
+
+    public Vector3[] itemsSpawnSpots;
+    public float GizmosSize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        for (int i = 0; i < itemsSpawnSpots.Length; i++)
+        {
+            Gizmos.DrawSphere(itemsSpawnSpots[i], GizmosSize);
+        }
+        
+    }
+}
