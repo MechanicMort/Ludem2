ca20b92b4d627daab4cb0809376d3214f56dd885
MechanicMort <47052071+MechanicMort@users.noreply.github.com>
2019-04-29 10:30:14 +0100

    asd

---
+++ LudumDare/Assets/Scenes/Joe/MenuScene/MainMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject menu;
+    public GameObject control;
+    
+
+
+    public void startGame()// set next scene number
+    {
+        FadeToLevel();        
+    }
+
+    public void FadeToLevel() // declare varibles and run animation
+    {
+        anim.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete() // load next level
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    
+
+
+
+    public void controls() // open the controls
+    {
+        flash();
+        menu.SetActive(false);        
+        control.SetActive(true);
+    }
+
+    public void returnMenu()// open the menu
+    {
+        flash();        
+        control.SetActive(false);
+        menu.SetActive(true);
+    }
+    
+    
+
+    public void flash() // hold animation for screen flashing black
+    {
+        anim.SetTrigger("BlackFlash");
+    }
+
+
+
+
+
+
+
+    public void quit() // allow the user to quit the game
+    {
+        Debug.Log("Quit");
+        anim.SetTrigger("Quit");
+    }
+
+    public void OnFadeQuit()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ LudumDare/Assets/Scenes/Joe/PauseMenu/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // set a bool for stating if the game is paused or not
+    public static bool Paused = false;
+    public GameObject pauseMenuUI;
+    public Animator anim;
+
+    
+
+	
+	// check to see if ESC key is pressed and what state the paused bool is in
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                ResumeGame();
+            }
+
+            else
+            {
+                PauseGame();
+            }
+        }
+	}
+
+    //press ESC key during paused and return to game button
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    //press ESC key to pause game
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void ReturnMenu()
+    {
+        Debug.Log("Menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
